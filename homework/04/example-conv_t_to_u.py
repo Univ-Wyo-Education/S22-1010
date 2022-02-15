8a9f8625435ec306ccba61852ca1f2aed90d315e
Philip Schlump <pschlump@gmail.com>
2022-02-15 02:40:16 -0700

    Update

---
+++ homework/04/example-conv_t_to_u.py
@@ -0,0 +1,20 @@
+# Convert DNA's T to RNA's U for later use in the code.  Also convert all to lower case.
+
+def conv_t_to_u(s) :
+    # Your Code At This Point - Convert string from Upper To Lower Case.
+    # Your Code At This Point - Replace "t" with "u" in string.
+    return s
+
+
+
+# Automated Test
+if __name__ == "__main__":
+    n_err = 0
+
+    # Add you test code - call conv_t_to_u
+    # Add your check that this is correct.
+
+    if n_err == 0 :
+        print ( "PASS" )
+    else:
+        print ( "FAILED" )
